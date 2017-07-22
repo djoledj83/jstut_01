f6d29118f6076484ed058206b7f38e742002b011
djoledj83 <30376767+djoledj83@users.noreply.github.com>
2017-07-22 22:31:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 <html>
 <body>
 
-<h2>My First JavaScript</h2>
+<h2>Prikazi datum</h2>
 
 <button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
+Prikazi datum i vreme.</button>
 
 <p id="demo"></p>
 
